bc4bdcd375974e747c03a33db6d450797057c6f7
mbraco-blip <m.braco@unesp.com>
2026-05-26 21:53:14 -0300

    reestrutura para pasta api

---
+++ api/index.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import pymysql
+import os
+
+load_dotenv()
+app = Flask(__name__)
+
+def get_db():
+    return pymysql.connect(
+        host=os.getenv('MYSQL_HOST'),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        database=os.getenv('MYSQL_DB'),
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.route('/')
+def index():
+    return jsonify({'status': 'ok', 'msg': 'API CulturaTur no ar!'})
+
+@app.route('/api/servicos', methods=['GET'])
+def listar():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM servicos ORDER BY criado_em DESC")
+    rows = cur.fetchall()
+    db.close()
+    return jsonify(rows)
+
+@app.route('/api/servicos', methods=['POST'])
+def cadastrar():
+    d = request.json
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("""INSERT INTO servicos
+        (nome,categoria,cidade,descricao,preco,horario,contato,avaliacao)
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",
+        (d['nome'],d['categoria'],d.get('cidade',''),
+         d.get('descricao',''),d.get('preco',''),
+         d.get('horario',''),d.get('contato',''),
+         d.get('avaliacao',4.0)))
+    db.commit()
+    db.close()
+    return jsonify({'id': cur.lastrowid, 'msg': 'Cadastrado!'}), 201
+
+@app.route('/api/servicos/<int:id>', methods=['DELETE'])
+def excluir(id):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("DELETE FROM servicos WHERE id=%s", (id,))
+    db.commit()
+    db.close()
+    return jsonify({'msg': 'Excluído!'})
